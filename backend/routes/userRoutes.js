a169e7075f0ff8c06fcd90ce1c3bf94aeaf943b8
deepcu-it <deepghosal445@gmail.com>
2024-07-31 22:56:01 +0530

    add token request

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -18,13 +18,14 @@ const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
-router.route("/update/me").put(updateUser);
-router.route("/update/password").put(updatePassword);
+router.route("/update/me").put(updateUser);//
+router.route("/update/password").put(updatePassword);//
 router.route("/reset-password").put(ressetPasswordByOTP);
-router.route("/me").get(getMe);
-router.route("/forgot-password").post(forgotPassword);
-router.route("/admin/delete/:id").delete(adminDeleteUser);
-router.route("/admin/getuser/:id").get(adminGetUser);
-router.route("/admin/get-all-user").get(adminGetAllUsers);
+router.route("/me").get(getMe);//
+router.route("/forgot-password").post(forgotPassword);//
+
+router.route("/admin/delete/:id").delete(adminDeleteUser);////
+router.route("/admin/getuser/:id").get(adminGetUser);////
+router.route("/admin/get-all-user").get(adminGetAllUsers);////
 
 export default router;
