1223a766d623aa68b9946458857caa912eb8bd43
deepcu-it <deepghosal445@gmail.com>
2024-08-20 01:15:48 +0530

    home page added carousel images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,23 @@ import RegisterPage from './pages/RegisterPage.js';
 import Test from "./Test.js";
 import ForgotPasswordForm from './components/Auth/ForgotPasswordForm.js';
 import ResetPasswordForm from './components/Auth/ResetPasswordForm.js';
+import Navbar from './components/Common/Navbar.js';
+import Footer from './components/Common/Footer.js';
+import HomePage from './pages/HomePage.js';
 function App() {
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/forgot-password" element={<ForgotPasswordForm />} />
                 <Route path="/reset-password" element={<ResetPasswordForm/>} />
+                <Route path="/home" element={<HomePage />} />
+
                 <Route path="/test" element={<Test/>}/>
             </Routes>
+            <Footer/>
         </Router>
     );
 }
