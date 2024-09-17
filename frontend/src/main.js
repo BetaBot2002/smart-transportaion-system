feaa2c6210c25e19f136b14f5d76d911b9fe14e0
deepcu-it <deepghosal445@gmail.com>
2024-09-17 20:10:52 +0530

    store added to app component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
-import { ChakraProvider } from '@chakra-ui/react'; // Import ChakraProvider
+import { ChakraProvider } from '@chakra-ui/react'; 
+import {Provider} from "react-redux"
+import store from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider> 
-      <App />
+      <Provider store={store} >
+          <App/>
+      </Provider>
     </ChakraProvider>
   </StrictMode>
 );
