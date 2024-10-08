94fd1553dda38d4b24687624648f3fec05dcaaea
deepcu-it <deepghosal445@gmail.com>
2024-10-09 00:26:10 +0530

    route changed for buttons

--- frontend/src/pages/Home/ContactUs.js
+++ frontend/src/pages/Home/ContactUs.js
@@ -1,9 +1,57 @@
-import { Box, Heading, Stack, Input, Textarea, Button } from '@chakra-ui/react';
+import { Box, Heading, Stack, Input, Textarea, Button, useToast } from '@chakra-ui/react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { bounceVariants } from './HomePage.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { clearUpdation, contactUsAction } from '../../redux/actions/userActions.js';
 
 
 export default function ContactUsForm() {
+
+    const {loading,isMessageSent,error} = useSelector(state => state.IsUpdatedUser);
+    const [email,setEmail] = useState("");
+    const [message,setMessage] = useState("");
+    const [subject,setSubject] = useState("");
+    const toast = useToast();
+    const dispatch = useDispatch();
+    const handleSubmit = ()=>{
+        if(email==="" ||message==="" || subject==="") {
+            toast({
+                title: 'invalid',
+                description: "Enter all details",
+                status: 'error',
+                duration: 3000,
+                isClosable: true
+            })
+            return;
+        }
+        dispatch(contactUsAction({
+            email:email,
+            subject:subject,
+            message:message
+        }))
+    }
+    useEffect(()=>{
+        if(isMessageSent) {
+            toast({
+                title: 'success',
+                description: "Feedback sent successfully",
+                status: 'success',
+                duration: 3000,
+                isClosable: true
+            })
+        }
+        if(error) {
+            toast({
+                title: 'invalid',
+                description: error,
+                status: 'error',
+                duration: 3000,
+                isClosable: true
+            })
+            dispatch(clearUpdation());
+        }
+    },[isMessageSent,error])
     return (
         <AnimatePresence>
             <motion.div
@@ -18,28 +66,26 @@ export default function ContactUsForm() {
                     </Heading>
                     <Stack spacing={4}>
                         <Input
-                            placeholder="Name"
-                            bg={'gray.100'}
-                            borderRadius={'md'}
-                        />
-                        <Input
                             placeholder="Email"
                             type="email"
                             bg={'gray.100'}
                             borderRadius={'md'}
+                            onChange={(e)=>setEmail(e.target.value)}
                         />
                         <Input
                             placeholder="Subject"
                             bg={'gray.100'}
                             borderRadius={'md'}
+                            onChange={(e)=>setSubject(e.target.value)}
                         />
                         <Textarea
                             placeholder="Message"
                             bg={'gray.100'}
                             borderRadius={'md'}
                             minHeight={'150px'}
+                            onChange={(e)=>setMessage(e.target.value)}
                         />
-                        <Button colorScheme="blue" type="submit">
+                        <Button isLoading={loading} colorScheme="blue" onClick={handleSubmit}>
                             Send Message
                         </Button>
                     </Stack>
