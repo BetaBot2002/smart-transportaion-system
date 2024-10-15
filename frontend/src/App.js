d592b2f928b6651e0cda2cacafcdd180a89da21c
deepcu-it <deepghosal445@gmail.com>
2024-10-15 20:35:32 +0530

    initialization of App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,29 +19,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllStations, getLRUtrains } from './redux/actions/trainActions.js';
 import ManageUser from './components/Dashboard/Admin/ManageUser.js';
 import ManageStation from './components/Dashboard/Admin/ManageStation.js';
-import GetShortestPath  from './components/Train/GetShortestPath.js';
+import GetShortestPath from './components/Train/GetShortestPath.js';
 import { Alert, AlertIcon, Box, Spinner } from '@chakra-ui/react';
 import PageNotFound from './pages/PageNotFound.js';
-import {  GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import CompleteRegistration from './components/Auth/CompleteRegistration.js';
+import ProtectedRoute from './protectedRoutes.js';
 
 
 function App() {
     const dispatch = useDispatch();
     const refreshToken = localStorage.getItem("refreshToken");
+
+    const { loading: loading1, error: err2, data: data1 = [] } = useSelector(state => state.GetAllStation);
     useEffect(() => {
         if (refreshToken) {
             dispatch(getProfileAction());
-            dispatch(getLRUtrains());
         }
         dispatch(getAllStations());
-    }, [])
-	const { loading: loading1, error: err2, data: data1 = [] } = useSelector(state => state.GetAllStation);
-    const { loading: loading2, data: trains, error: err1 } = useSelector(state => state.GetSearchHistory);
 
-    function GoogleLogin () {
+    }, [])
+    function GoogleLogin() {
         return (
-			<GoogleOAuthProvider clientId="146756462672-ldk10gufg7c3v9jun37p6hme4he803ia.apps.googleusercontent.com">
-                <LoginPage/>
+            <GoogleOAuthProvider clientId="146756462672-ldk10gufg7c3v9jun37p6hme4he803ia.apps.googleusercontent.com">
+                <LoginPage />
             </GoogleOAuthProvider>
         )
     }
@@ -50,35 +51,39 @@ function App() {
         <Router>
             <Alert status='info'>
                 <AlertIcon />
-                Website is under construction. If you find any error fill contact us form!
+                For the first you need to wait for 60 seconds since we have deployed backend free.
+                If ther is any error please reach out us via contact us form in home page
             </Alert>
             <Navbar />
-            {loading1 || loading2 ? (
-			<Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-				<Spinner size="xl" />
-			</Box>
-		) :
-            <Routes>
-                <Route path='*' element={<PageNotFound/>}/>
-                <Route path="/login" element={<GoogleLogin />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/forgot-password" element={<ForgotPasswordForm />} />
-                <Route path="/verify-otp" element={<VerifyOtp />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="/routes" element={<RoutePage />} />
-                <Route path="/get-shortest-path" element={<GetShortestPath />} />
+            {loading1 ? (
+                <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+                    <Spinner size="xl" />
+                </Box>
+            ) :
+                <Routes>
+                    <Route path='*' element={<PageNotFound />} />
+                    <Route path="/login" element={<GoogleLogin />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/complete-profile" element={<CompleteRegistration />} />
+                    <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+                    <Route path="/verify-otp" element={<VerifyOtp />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/routes" element={<RoutePage />} />
+                    <Route path="/get-shortest-path" element={<GetShortestPath />} />
+                    <Route path="/About Us" element={<AboutUs />} />
 
-                <Route path="/profile" element={<UserProfile />} />
-                <Route path="/About Us" element={<AboutUs />} />
-                <Route path="/admin/manage-user" element={<ManageUser />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path='/change-password' element={<ChangePassword />} />
-                <Route path='/saved-routes' element={<ViewFavouriteRoutes />} />
-                <Route path="/admin/manage-station" element={<ManageStation />} />
 
-            </Routes>
-            }  
+                    <Route path="/profile" element={<ProtectedRoute element={<UserProfile />} />} />
+                    <Route path="/admin/manage-user" element={<ProtectedRoute element={<ManageUser />} />} />
+                    <Route path="/logout" element={<ProtectedRoute element={<Logout />} />} />
+                    <Route path="/reset-password" element={<ProtectedRoute element={<ChangePassword />} />} />
+                    <Route path="/saved-routes" element={<ProtectedRoute element={<ViewFavouriteRoutes />} />} />
+                    <Route path="/admin/manage-station" element={<ProtectedRoute element={<ManageStation />} />} />
+
+
+                </Routes>
+            }
             <Footer />
         </Router>
     );
