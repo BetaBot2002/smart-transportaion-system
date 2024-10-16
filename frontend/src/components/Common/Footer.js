9c47f911435b78ed6e75242870c0da7f7df6286e
deepcu-it <deepghosal445@gmail.com>
2024-10-16 14:19:33 +0530

    href changed to navigate

--- frontend/src/components/Common/Footer.js
+++ frontend/src/components/Common/Footer.js
@@ -6,15 +6,14 @@ import {
   Stack,
   SimpleGrid,
   Text,
-  VisuallyHidden,
-  chakra,
   useColorModeValue
 } from '@chakra-ui/react'
 import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa'
-
-
+import { useNavigate } from 'react-router-dom'
 
 export default function Test() {
+  const navigate = useNavigate();
+
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
@@ -23,63 +22,33 @@ export default function Test() {
         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={10}>
           <Stack align={'center'}>
             <Text>Company</Text>
-            <Box as="a" href={'#'}>
-              About Us
-            </Box>
-            <Box as="a" href={'#'}>
-              Blog
-            </Box>
-            <Box as="a" href={'#'}>
-              Careers
-            </Box>
-            <Box as="a" href={'#'}>
-              Contact Us
-            </Box>
+            <Text cursor="pointer" onClick={() => navigate('#')}>About Us</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Blog</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Careers</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Contact Us</Text>
           </Stack>
 
           <Stack align={'center'}>
             <Text>Support</Text>
-            <Box as="a" href={'#'}>
-              Help Center
-            </Box>
-            <Box as="a" href={'#'}>
-              Safety Center
-            </Box>
-            <Box as="a" href={'#'}>
-              Community Guidelines
-            </Box>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Help Center</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Safety Center</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Community Guidelines</Text>
           </Stack>
 
           <Stack align={'center'}>
             <Text>Legal</Text>
-            <Box as="a" href={'#'}>
-              Cookies Policy
-            </Box>
-            <Box as="a" href={'#'}>
-              Privacy Policy
-            </Box>
-            <Box as="a" href={'#'}>
-              Terms of Service
-            </Box>
-            <Box as="a" href={'#'}>
-              Law Enforcement
-            </Box>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Cookies Policy</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Privacy Policy</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Terms of Service</Text>
+            <Text cursor="pointer" onClick={() => navigate('#')}>Law Enforcement</Text>
           </Stack>
 
           <Stack align={'center'}>
             <Text>Features</Text>
-            <Box as="a" href={'/home'}>
-              Home
-            </Box>
-            <Box as="a" href={'/trains'}>
-              Trains
-            </Box>
-            <Box as="a" href={'/stations'}>
-              Stations
-            </Box>
-            <Box as="a" href={'/routes'}>
-              Routes
-            </Box>
+            <Text cursor="pointer" onClick={() => navigate('/home')}>Home</Text>
+            <Text cursor="pointer" onClick={() => navigate('/trains')}>Trains</Text>
+            <Text cursor="pointer" onClick={() => navigate('/stations')}>Stations</Text>
+            <Text cursor="pointer" onClick={() => navigate('/routes')}>Routes</Text>
           </Stack>
 
         </SimpleGrid>
@@ -99,9 +68,9 @@ export default function Test() {
           align={{ md: 'center' }}>
           <Text>© {new Date().getFullYear()} Smart Transportation. All rights reserved</Text>
           <Stack direction={'row'} spacing={6}>
-              <FaTwitter />
-              <FaYoutube />
-              <FaInstagram />
+            <FaTwitter />
+            <FaYoutube />
+            <FaInstagram />
           </Stack>
         </Container>
       </Box>
