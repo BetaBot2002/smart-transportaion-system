728dce0258a9dbc0ca9ac4bace7b04d346e40a47
deepcu-it <deepghosal445@gmail.com>
2024-10-20 15:23:41 +0530

    user waiting problem solved

--- frontend/src/components/Common/AlertNav.js
+++ frontend/src/components/Common/AlertNav.js
@@ -3,14 +3,12 @@ import { useSelector } from "react-redux"
 
 
 export const AlertNav = () => {
-    const {isAuthenticated,user} = useSelector(state => state.GetUser);
+    const { isAuthenticated, user } = useSelector(state => state.GetUser);
     return (
-        isAuthenticated && <Alert status='info'>
+        isAuthenticated && !user.isEmailVerified && <Alert status='info'>
             <AlertIcon />
-            { user && user.isEmailVerified? 
-            <Text> For Free version you have to wait 60 seconds for first visit. If there is any error please reach out us via contact us form in home page </Text>
-            : <Text>Please <Text as='a' textDecoration={'underline'} href="/verify-email" >verify</Text> this e-mail</Text>
-            }
+
+            <Text>Please <Text as='a' textDecoration={'underline'} href="/verify-email" >verify</Text> this e-mail</Text>
         </Alert>
     )
 }
